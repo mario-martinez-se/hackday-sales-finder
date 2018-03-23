01cc4f2d06f9637aebd5f68eac075c98c49d9f6d
Glitch (bronze-canoe) <none>
2018-03-23 11:43:40 +0000

    Search sales async

--- index.js
+++ index.js
@@ -18,7 +18,16 @@ app.set('view engine', 'handlebars');
 app.get('/', (request, response) =>{
 	response.render('home');
 });
-
+app.get('/sales', (request, response) => {
+  const query = request.query.query;
+  if (query !== undefined) {
+    console.log(`Searching ${query} in SE API`);
+    rp(getSEPostRequestForQuery(query))
+    .then(data => response.send(data));
+  } else {
+    response.send("Error: Query was empty");
+  }
+});
 app.post('/find', (request, response) => {
 	rp(request.body.url_analyse)
 	.then(data => rp (getGCPNLRequestForQuery(data))
@@ -30,28 +39,7 @@ app.post('/find', (request, response) => {
 		const country = (address_components.find(address_component => _.any(address_component.types, type => type == 'country')) || {long_name: undefined}).long_name;
 		const city = _.max(address_components.filter(address_component => _.any(address_component.types, type => /^administrative_area_level_(\d)/g.exec(type) != null)).map(address_component => ({long_name: address_component.long_name, type: address_component.types.find(type => /^administrative_area_level_(\d)/g.exec(type) != null)})), address_component => /^administrative_area_level_(\d)/g.exec(address_component.type)[1]).long_name || undefined;
 
-		var requests = [];
-		if (country) {
-			requests.push(rp(getSEPostRequestForQuery(city)));
-		} else {
-			requests.push(Promise.resolve(undefined));
-		}
-
-		if (city) {
-			requests.push(rp(getSEPostRequestForQuery(country)));
-		} else {
-			requests.push(Promise.resolve(undefined));
-		}
-
-		return  Promise.all(_.union(requests, [Promise.resolve(city), Promise.resolve(country)]));
-	})
-	.then(data => {
-		const salesForCity = (data[0] || {match: undefined}).match;
-		const salesForCountry = (data[1] || {match: undefined}).match;
-		const city = data[2];
-		const country = data[3];
-		response.render('find_results', {salesForCity: salesForCity || [], salesForCountry: salesForCountry || [], city: city, country: country})
-
+    response.render('find_results', {salesForCity: [], salesForCountry: [], city: city, country: country})
 	})
 	.catch(err => {
 	console.error('ERROR:', err);
